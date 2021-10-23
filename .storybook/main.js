530fab6af54fe857c0788f6e0b8c1e08e040387d
hhhtak <matuyamen@hotmail.co.jp>
2021-10-23 14:08:50 +0900

    setting storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,22 @@
+const path = require("path")
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"]
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "../src/components"),
+    }
+    return config
+  },
+  // paths: {
+  //   '@components/*': ['../src/components/*'],
+  // },
+  // resolve: {
+  //   alias: {
+  //     '@components': path.join(__dirname, '../src/components/*'),
+  //   },
+  // },
 }
