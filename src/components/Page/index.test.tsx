c9ec6002e02199066adf2f110b2e57d413e87854
hhhtak <matuyamen@hotmail.co.jp>
2021-11-26 08:53:49 +0900

    add test

---
+++ src/components/Page/index.test.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react'
+import { composeStories } from '@storybook/testing-react'
+import * as stories from '@components/Page/index.stories'
+
+const { LoggedIn, LoggedOut } = composeStories(stories)
+
+test('LoggedIn storybook test', () => {
+  const { getByText } = render(<LoggedIn />)
+  const text = getByText('Log out')
+  expect(text).not.toBeNull()
+})
+
+test('LoggedOut storybook test', () => {
+  const { getByText } = render(<LoggedOut />)
+  const text = getByText('Log in')
+  expect(text).not.toBeNull()
+})
