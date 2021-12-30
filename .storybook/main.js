9d02891f0af7a2f5d5e5944680a0bad31243e064
hhhtak <matuyamen@hotmail.co.jp>
2021-12-30 22:22:05 +0900

    add useCase path

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
       ...config.resolve.alias,
       '@components': path.resolve(__dirname, '../src/components'),
       '@assets': path.resolve(__dirname, '../src/assets'),
+      '@useCase': path.resolve(__dirname, '../src/useCase'),
     }
     return config
   },
