4012f8a2c4a9329ca2e485282f64798310eb0590
hhhtak <matuyamen@hotmail.co.jp>
2022-01-11 09:26:13 +0900

    helloテスト追加

---
+++ src/pages/api/hello/index.test.tsx
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import httpMocks from 'node-mocks-http'
+import handler from '@api/hello'
+
+describe('/api/hello', () => {
+  test('200', async () => {
+    const mockReq: any = httpMocks.createRequest()
+    const mockRes: any = httpMocks.createResponse()
+
+    await handler(mockReq, mockRes)
+    expect(mockRes.statusCode).toEqual(200)
+  })
+})
