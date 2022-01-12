ee52d1a3f8e53bcd76d00aa438e59d15f50172e1
hhhtak <matuyamen@hotmail.co.jp>
2022-01-12 09:51:09 +0900

    api/usersAPI修正

--- src/pages/api/users/index.test.tsx
+++ src/pages/api/users/index.test.tsx
@@ -1,22 +1,21 @@
+import nock from 'nock'
+import fetch from 'node-fetch'
 import httpMocks from 'node-mocks-http'
 import handler from '@api/users'
 
 describe('/api/users', () => {
-  const OLD_ENV = process.env
-
-  beforeEach(() => {
-    jest.resetModules() // Most important - it clears the cache
-    process.env = { ...OLD_ENV } // Make a copy
-  })
-
-  afterAll(() => {
-    process.env = OLD_ENV // Restore old environment
-  })
-
   test('200', async () => {
+    ;(globalThis as any).fetch = fetch
+
+    process.env.USERS_API_PATH = 'http://api.com/users'
+    const mockData = { name: 'aaaaaaaa' }
+    nock('http://api.com').get('/users').reply(200, mockData)
+
     const mockReq: any = httpMocks.createRequest()
     const mockRes: any = httpMocks.createResponse()
     await handler(mockReq, mockRes)
+
     expect(mockRes.statusCode).toEqual(200)
+    expect(mockRes._getJSONData()).toEqual(mockData)
   })
 })
