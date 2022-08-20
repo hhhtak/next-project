e9b5d371b47a6b0a2e59aa670cb740cdf60c356f
takamatsu-hideomi <matuyamen@hotmail.co.jp>
2022-08-20 12:50:31 +0900

    test追加 不要は設定、ファイルを削除

--- src/components/atoms/Button/index.test.tsx
+++ src/components/atoms/Button/index.test.tsx
@@ -1,24 +1,40 @@
 import { render, screen } from '@testing-library/react'
-import { composeStories } from '@storybook/testing-react'
-import * as stories from './index.stories' // import all stories from the stories file
+import 'jest-styled-components'
+import Button, { ButtonProps } from '@components/atoms/Button'
+import { ButtonSize } from '@components/atoms/Button/styled'
 
-// Every component that is returned maps 1:1 with the stories, but they already contain all decorators from story level, meta level and global level.
-const { Primary, Secondary } = composeStories(stories)
+describe('Button Component', () => {
+  const text = 'Button Component'
+  const buttonSize: ButtonSize = 'small'
+  const mockProps: ButtonProps = { size: buttonSize, children: text }
 
-test('renders primary button with default args', () => {
-  render(<Primary />)
-  const buttonElement = screen.getByText(/foo/i)
-  expect(buttonElement).not.toBeNull()
-})
+  it('childrenに渡した値が正しく表示されていること', () => {
+    const { container } = render(<Button {...mockProps} />)
+    screen.debug()
+    expect(screen.getByText(text)).toBeTruthy()
+    expect(container.getElementsByClassName(buttonSize)).toBeTruthy()
+  })
 
-test('renders primary button with overridden props', () => {
-  render(<Primary>Hello world</Primary>) // you can override props and they will get merged with values from the Story's args
-  const buttonElement = screen.getByText(/Hello world/i)
-  expect(buttonElement).not.toBeNull()
-})
+  it('props.size未設定の場合、mediumが設定されること', () => {
+    const mediumButtonSize: ButtonSize = 'medium'
+    const props: ButtonProps = { children: text }
+    const { container } = render(<Button {...props} />)
+    expect(container.getElementsByClassName(mediumButtonSize)).toBeTruthy()
+  })
 
-test('Secondary', () => {
-  const { container } = render(<Secondary />)
-  const buttonElement = screen.getByRole('button')
-  expect(buttonElement.textContent).toEqual(Secondary.args?.children)
+  it('props.size:largeの場合、largeが設定されること', () => {
+    const largeButtonSize: ButtonSize = 'large'
+    const props: ButtonProps = { ...mockProps, size: largeButtonSize }
+    const { container } = render(<Button {...props} />)
+    expect(container.getElementsByClassName(largeButtonSize)).toBeTruthy()
+  })
+
+  it('props.primary:true場合、largeが設定されること', () => {
+    const props: ButtonProps = { ...mockProps, primary: true }
+    const { container } = render(<Button {...props} />)
+    console.log(container.firstChild)
+    expect(container.firstChild).toHaveStyleRule('color', 'white', {})
+    expect(container.firstChild).toHaveStyleRule('background-color', '#1ea7fd', {})
+    expect(container.firstChild).toHaveStyleRule('box-shadow', 'none', {})
+  })
 })
