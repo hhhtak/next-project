6164bb035210ba2c991a74017fc432bbb299fbd9
takamatsu-hideomi <matuyamen@hotmail.co.jp>
2022-08-27 17:11:52 +0900

    テスト修正

--- src/components/atoms/ContextButton/index.test.tsx
+++ src/components/atoms/ContextButton/index.test.tsx
@@ -3,13 +3,11 @@ import 'jest-styled-components'
 import ContextButton from '@components/atoms/ContextButton'
 
 describe('ContextButton Component', () => {
-  it('ボタン押下でテキストがインクリメントされること', () => {
+  it('ボタン押下でテキストがインクリメントされること', async () => {
     render(<ContextButton />)
     expect(screen.findByText('+1')).toBeTruthy()
-
-    screen.debug()
     const button = screen.getByRole('button')
-    fireEvent.click(button, {})
+    fireEvent.click(button, 'increment')
     expect(screen.findByText('+2')).toBeTruthy()
   })
 })
